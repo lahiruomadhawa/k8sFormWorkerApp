21bcc0d120c86e6a0d93319f362d70fd22e98db8
lahiruomadhawa <lahirumadhawa00@gmail.com>
2025-06-28 05:13:35 +1000

    Initial commit (create project)

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace k8sFormWorkerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
