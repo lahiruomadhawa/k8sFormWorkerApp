cfd9f4f3a72819cad37d09cd3563b0a46f354468
lahiruomadhawa <lahirumadhawa00@gmail.com>
2025-06-28 05:30:59 +1000

    Person model

---
+++ Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace k8sFormWorkerApp.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
